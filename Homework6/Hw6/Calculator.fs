2e25de5b57cc7b68eb06293601287cf117a3e49c
Ruzan Valeev <ruzan.valeeff@yandex.ru>
2023-10-15 15:29:15 +0300

    Completed 6-th homework

---
+++ Homework6/Hw6/Calculator.fs
@@ -0,0 +1,74 @@
+﻿module Hw6.Calculator
+
+open System
+
+type Message =
+    | SuccessfulExecution     = 0
+    | WrongArgLength          = 1
+    | WrongArgFormat          = 2
+    | WrongArgFormatOperation = 3
+    | DivideByZero            = 4
+
+type CalculatorOperation =
+     | Plus     = 0
+     | Minus    = 1
+     | Multiply = 2
+     | Divide   = 3
+
+type MaybeBuilder() =
+    member this.Bind(x, f): Result<'e,'d> =
+        match x with
+        | Ok s    -> f s
+        | Error e -> Error e
+        
+    member this.Return x: Result<'a,'b> =
+        Ok x
+    
+let maybe = MaybeBuilder()
+
+let isArgLengthSupported (args: string[]): Result<string * string * string, Message> =
+    match args.Length = 3 with
+    | true  -> Ok (args[0], args[1], args[2]) 
+    | false -> Message.WrongArgLength |> Error
+
+let inline isOperationSupported (arg1, operation, arg2): Result<('a * CalculatorOperation * 'b), Message> =
+    match operation with
+    | "+" | "плюс"                   -> Ok (arg1, CalculatorOperation.Plus, arg2)
+    | "-" | "минус"                  -> Ok (arg1, CalculatorOperation.Minus, arg2)
+    | "*" | "умножить"               -> Ok (arg1, CalculatorOperation.Multiply, arg2)
+    | "/" | "разделить" | "поделить" -> Ok (arg1, CalculatorOperation.Divide, arg2)
+    | _   -> Message.WrongArgFormatOperation |> Error
+
+let parseArgs (arg1: string, operation, arg2: string): Result<(Decimal * CalculatorOperation * Decimal), Message> =
+    match arg1 |> Decimal.TryParse with
+    | false, _   -> Message.WrongArgFormat |> Error
+    | true, arg1 ->
+        match arg2 |> Decimal.TryParse with 
+        | false, _   -> Message.WrongArgFormat |> Error
+        | true, arg2 -> Ok (arg1, operation, arg2)
+
+let inline isDividingByZero (arg1, operation, arg2): Result<('a * CalculatorOperation * 'b), Message> =
+    match operation with
+    | CalculatorOperation.Divide ->
+        match int arg2 with
+        | 0 -> Message.DivideByZero |> Error
+        | _ -> Ok (arg1, operation, arg2)
+    | _ -> Ok (arg1, operation, arg2)
+
+let parseCalcArguments (args: string[]): Result<(Decimal * CalculatorOperation * Decimal), Message> =
+    maybe {
+        let! x      = isArgLengthSupported args
+        let! y      = isOperationSupported x
+        let! z      = parseArgs y
+        let! result = isDividingByZero z
+        return result
+    }
+   
+let inline calculate value1 (operation : CalculatorOperation) value2 : ^a =
+    match int operation with
+    | 0 -> value1 + value2
+    | 1 -> value1 - value2
+    | 2 -> value1 * value2
+    | 3 -> value1 / value2
+    | _ -> LanguagePrimitives.GenericZero
+    
--- Homework6/Hw6/Program.fs
+++ Homework6/Hw6/Program.fs
@@ -1,26 +1,95 @@
 module Hw6.App
 
-open System
+open Hw6.Calculator
 open Microsoft.AspNetCore.Hosting
 open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Http
 open Microsoft.Extensions.Hosting
 open Microsoft.Extensions.Logging
 open Microsoft.Extensions.DependencyInjection
 open Giraffe
+open Giraffe.ViewEngine
 
-let calculatorHandler: HttpHandler =
-    fun next ctx ->
-        let result: Result<string, string> = raise(NotImplementedException())
+let masterPage (pageTitle : string) (content : XmlNode list) =
+    html [] [
+        head [] [
+            meta [ _charset "utf-8"
+                   _name "viewport"
+                   _content "width=device-width, initial-scale=1" ]
+            title [] [ str pageTitle ]
+            link [ _href "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+                   _rel "stylesheet"
+                   _integrity "sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+                   _crossorigin "anonymous" ]
+            script [ _src "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+                     _integrity "sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+                     _crossorigin "anonymous" ] []
+            script [ _src "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+                     _integrity "sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+                     _crossorigin "anonymous" ] []
+        ]
+        body [ _class "container" ] [
+            h1 [ _class "text-center" ] [ str pageTitle ]
+            main [] content
+        ]
+    ]
 
-        match result with
-        | Ok ok -> (setStatusCode 200 >=> text (ok.ToString())) next ctx
-        | Error error -> (setStatusCode 400 >=> text error) next ctx
-
+let calculatePage =
+    [
+        h5 [] [ str "Памятка: Используйте ',' для десятичных чисел" ]
+        form [ _class "row g-2"; _action "/calculate"; _method "POST"; _id "calculator" ] [
+            div [ _class "col-md-5" ] [
+                label [ _class "form-label" ] [ str "Первое число:" ]
+                input [ _class "form-control"; _type "text"; _name "value1" ]
+            ]
+            div [ _class "col-md-2" ] [
+                label [ _class "form-label" ] [ str "Операция:" ]
+                input [ _class "form-control"; _type "text"; _name "operation" ]
+            ]
+            div [ _class "col-md-5" ] [
+                label [ _class "form-label" ] [ str "Второе число:" ]
+                input [ _class "form-control"; _type "text"; _name "value2" ]
+            ]
+            button [ _class "btn btn-success"; _type "submit"] [ str "Посчитать!" ]
+        ]   
+    ] |> masterPage "Калькулятор"
+  
+[<CLIMutable>]
+type CalculatorArguments =
+    {
+        Value1 : string
+        Operation : string
+        Value2 : string
+    }
+   
+let calculatorHandler : HttpHandler =
+    fun (next : HttpFunc) (ctx : HttpContext) ->
+        task {
+            let! model = ctx.BindFormAsync<CalculatorArguments>()
+            let parsed = parseCalcArguments([|model.Value1; model.Operation; model.Value2|])
+            let result =
+                match parsed with
+                | Ok s ->
+                    let arg1, op, arg2 = s
+                    Ok ((calculate arg1 op arg2).ToString()) 
+                | Error e -> Error (e.ToString())
+            
+            match result with
+            | Ok ok       -> return! (setStatusCode 200 >=> text ("Результат: " + ok.ToString())) next ctx
+            | Error error -> return! (setStatusCode 400 >=> text ("Ошибка: " + error)) next ctx
+        }
+        
 let webApp =
     choose [
-        GET >=> choose [
-             route "/" >=> text "Use //calculate?value1=<VAL1>&operation=<OPERATION>&value2=<VAL2>"
-        ]
+        GET >=>
+            choose [
+                 route "/"           >=> text "index page"
+                 route "/calculator" >=> htmlView calculatePage
+            ]
+        POST >=>
+            choose [
+                 route "/calculate"  >=> calculatorHandler
+            ]
         setStatusCode 404 >=> text "Not Found" 
     ]
     
